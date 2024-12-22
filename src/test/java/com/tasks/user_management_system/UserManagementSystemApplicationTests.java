26cb20325fefbf4de0db88eccebf1da140120056
almog <almogs575@gmail.com>
2024-12-22 19:08:49 +0200

    added swagger file

--- src/test/java/com/tasks/user_management_system/UserManagementSystemApplicationTests.java
+++ src/test/java/com/tasks/user_management_system/UserManagementSystemApplicationTests.java
@@ -2,7 +2,6 @@ package com.tasks.user_management_system;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
